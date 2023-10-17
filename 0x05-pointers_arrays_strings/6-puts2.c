670d7c2a235ec48581dbd09927953f74c904ab5d
BRIGHT NWABUNWENE <52702336+bryght001@users.noreply.github.com>
2023-10-17 05:32:43 +0100

    update 0x05-pointers_arrays_strings/6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,12 +17,12 @@ while (*y != '\0')
 y++;
 longi++;
 }
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
+t = longi - 1;
+for (o = 0 ; o <= t ; o++)
+{
+if (o % 2 == 0)
+{
+_putchar(str[o]);
 	}
 	}
 	_putchar('\n');
