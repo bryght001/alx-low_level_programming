82d86dbacc23d7a798d6a6d0974acc9150a84b59
BRIGHT NWABUNWENE <52702336+bryght001@users.noreply.github.com>
2023-10-17 05:22:42 +0100

    update 0x05-pointers_arrays_strings/4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,14 +12,14 @@ int o;
 while (*s != '\0')
 {
 longi++;
-	s++;
-	}
-	s--;
-	for (o = longi; o > 0; o--)
-	{
-		_putchar(*s);
-		s--;
-	}
+s++;
+}
+s--;
+for (o = longi; o > 0; o--)
+{
+_putchar(*s);
+s--;
+}
 
-	_putchar('\n');
+_putchar('\n');
 }
