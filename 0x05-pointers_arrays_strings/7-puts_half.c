8d48f1b3311f878b1a4c9dbeb2b8e8451979a880
BRIGHT NWABUNWENE <52702336+bryght001@users.noreply.github.com>
2023-10-17 05:35:00 +0100

    update 0x05-pointers_arrays_strings/7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,12 @@ int a, n, longi;
 
 longi = 0;
 
-	for (a = 0; str[a] != '\0'; a++)
-		longi++;
+for (a = 0; str[a] != '\0'; a++)
+longi++;
 
-	n = (longi / 2);
+n = (longi / 2);
 
-	if ((longi % 2) == 1)
+if ((longi % 2) == 1)
 		n = ((longi + 1) / 2);
 
 	for (a = n; str[a] != '\0'; a++)
