68ba53d8ec26eb7d51670abcb708c3bfc581c2d4
BRIGHT NWABUNWENE <52702336+bryght001@users.noreply.github.com>
2023-10-24 02:46:55 +0100

    update 0x07-pointers_arrays_strings/0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,8 @@
 include "main.h"
+
 /**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
+* _memset - fill a block of memory with a specific value
+* @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
  *
